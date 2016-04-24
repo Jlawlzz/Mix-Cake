2279c5278a3aa9b545ca70b90712e3d48b9069d9
Jordan Lawler <jordan.lawler@colorado.edu>
2016-04-24 15:47:05 -0600

    songs now store in redis store

---
+++ store.js
@@ -0,0 +1,35 @@
+'use strict';
+
+let redis = require('redis');
+let client = redis.createClient();
+
+
+let Store = function(){
+  this.tempStore = {}
+  this.currentSong = null
+}
+
+Store.prototype.initTemp = function(id){
+  this.tempStore[`${id}`] = []
+  this.currentSong = id
+}
+
+Store.prototype.updateTemp = function(message){
+  if (message['fft'] !== undefined){
+    this.tempStore[`${message['id']}`].push(message['fft'])
+  }
+}
+
+Store.prototype.logTemp = function(id){
+  if (this.currentSong !== null) {
+    client.set(`'${this.currentSong}'`, JSON.stringify(this.tempStore[`${this.currentSong}`]), function(err, reply) {
+      console.log(reply)
+    })
+  }
+}
+
+client.on('connect', function() {
+    console.log('connected');
+});
+
+module.exports = Store;
