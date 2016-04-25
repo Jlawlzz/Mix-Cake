09f909f3148d7ad1bbd76d7cd8272ecdaf653fbb
Jordan Lawler <jordan.lawler@colorado.edu>
2016-04-25 03:46:21 -0600

    updated package json

--- store.js
+++ store.js
@@ -3,7 +3,7 @@
 let client;
 
 if (process.env.REDISTOGO_URL) {
-  let rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  let rtg = require("url").parse(process.env.REDISTOGO_URL);
   client = require("redis").createClient(rtg.port, rtg.hostname);
   redis.auth(rtg.auth.split(":")[1]);
 } else {
