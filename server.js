baba915789e62442a2cde89c32fd9fa8a7a6d3a4
Jordan Lawler <Jordanalawler@gmail.com>
2017-02-02 22:12:58 -0800

    updated port

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ let server = http.createServer(app);
 const socketIo = require('socket.io');
 const io = socketIo(server);
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 8000;
 
 let store;
 
